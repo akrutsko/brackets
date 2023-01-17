de999774b759d74f100b7219b755468fd49dad7e
Aliaksei Krutsko <aliaksei_krutsko@epam.com>
2023-01-17 17:16:37 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  bracketsConfig.forEach(bracket => brackets.push(bracket.join('')));
+
+  let bracket;
+  const hasBracket = (str, brackets) => brackets.find(bracket => str.includes(bracket));
+
+  while (bracket = hasBracket(str, brackets)) {
+    str = str.replaceAll(bracket, '');
+  }
+  return !str.length;
 }
